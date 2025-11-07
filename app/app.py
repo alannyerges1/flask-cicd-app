2ce72d6d784526ca323a0652038ea853673220d2
Alan Nyerges <[alannyerges@msn.com]>
2025-11-07 14:52:12 -0500

    Updated homepage message text

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return jsonify(message="CI/CD pipeline test - new version deployed manually!")
+    return jsonify(message="CI/CD pipeline test - new version deployed manually!!")
 
 @app.route('/health')
 def health():
